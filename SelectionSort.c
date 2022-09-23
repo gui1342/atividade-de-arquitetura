af01d030ca4618ae38917172e9ca8723741fc423
gui1342 <103466943+gui1342@users.noreply.github.com>
2022-09-23 00:42:14 -0300

    Update SelectionSort.c

--- SelectionSort.c
+++ SelectionSort.c
@@ -3,29 +3,26 @@
 #define TAM 5
 
 int main(){
-  int lista[] = {50, 15, 2, 93, 15};
-  int i, j, indicemenor, temp, *listamenor, *listai;
+  int lista[] = {50, 2, 15, 93, 15};
+  int i, j, indicemenor, temp;
  
-    for (i = 0; i < TAM-1; i++){
-      indicemenor = i;
-      for (j = i+1; j < TAM; j++){
-        if (lista[j] < lista[indicemenor])
-          indicemenor = j;
-        }
-
-      if(indicemenor != i){
-        listamenor = &lista[indicemenor];
-        listai = &lista[i];
-      
-        temp = *listamenor;
-        *listamenor = *listai;
-        *listai = temp;
-        }
+  for (i = 0; i < TAM-1; i++){
+    indicemenor = i;
+    for (j = i+1; j < TAM; j++){
+      if (lista[j] < lista[indicemenor])
+        indicemenor = j;
     }
+
+    if(indicemenor != i){
+      temp = lista[indicemenor];
+      lista[indicemenor] = lista[i];
+      lista[i] = temp;
+    }
+  }
     for (int z = 0; z < TAM; z++){
-      printf("%d ", lista[z]);
-    printf("\n");
-      }
+      printf("%i ", lista[z]);
+      printf("\n");
+    }
   
 	return 0;
 }
